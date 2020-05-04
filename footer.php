cb81fb982af9a72dda7e50f7c2ff8fa492f255da
Badar Jamil <badarishere@gmail.com>
2020-05-05 02:48:56 +0500

    masterfiles of store villa

--- footer.php
+++ footer.php
@@ -1,18 +1,33 @@
 <?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Store_Villa
+ */
+
+?>
 	<?php if( !( is_home() || is_front_page() ) ) { ?>
 		</div>
-	</div> 
+	</div> <!-- Store Vill Container -->
 	<?php } ?>
 
 </div><!-- #content -->
 
-	<?php do_action( 'ecommerce_before_footer' ); ?>
+	<?php do_action( 'storevilla_before_footer' ); ?>
 	
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
 				<?php
-					
-					do_action( 'ecommerce_footer' ); 
+					/**
+					 * @hooked storevilla_footer_widgets - 10
+					 * @hooked storevilla_credit - 20
+					 * @hooked storevilla_payment_logo - 40
+					 */
+					do_action( 'storevilla_footer' ); 
 				?>		
 			
 		</footer><!-- #colophon -->
